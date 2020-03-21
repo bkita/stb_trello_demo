71e2aa3c928276bf67bea23fffa6db98c0b720d6
Bartosz Kita <bartosz.kita@gmail.com>
2020-03-21 13:59:46 +0100

    Removed trello key and token

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -12,8 +12,8 @@ public class BaseTest {
     protected static final String LISTS = "lists";
     protected static final String CARDS = "cards";
 
-    protected static final String KEY = "65fcb0686fdf1704d660ee5c7504b4de";
-    protected static final String TOKEN = "0954bbacc883b371ac891f47b0704718faf3b10b3c3d5ce4f1cd55c533e28f96";
+    protected static final String KEY = "YOUR_KEY";
+    protected static final String TOKEN = "YOUR_TOKEN";
 
     protected static RequestSpecBuilder reqBuilder;
     protected static RequestSpecification reqSpec;
